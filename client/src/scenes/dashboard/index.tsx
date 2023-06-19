00caaf9e4da890ec4a07fe4b4e265bd780997673
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-06-19 16:08:14 +0300

    Create Dashboard and Grid

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -2,10 +2,35 @@ import { Box, useTheme } from '@mui/material'
 
 type Props = {}
 
+const gridTemplate = `
+    "a b c"
+    "a b c"
+    "a b c"
+    "a b f"
+    "d e f"
+    "d e f"
+    "d h i"
+    "g h i"
+    "g h j"
+    "g h j"
+
+`;
+
 const Dashboard = (props: Props) => {
     const { palette } = useTheme();
   return (
-    <Box color={palette.grey[300]}>Dashboard</Box>
+    <Box 
+    width="100%" 
+    height="100%" 
+    display="grid" 
+    gap="1.5rem"
+    sx={{
+        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+        gridTemplateAreas: gridTemplate,
+    }}
+    >
+        Dashboard
+    </Box>
   )
 }
 
