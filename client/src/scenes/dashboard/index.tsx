9fd369beb471fafc56c1c4e1245afe9b1b3521de
zlatanov-dev <zlatanov.yasen@gmail.com>
2023-06-19 16:32:12 +0300

    Grid Configuration

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,37 +1,46 @@
-import { Box, useTheme } from '@mui/material'
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { gridTemplateLargeScreens, gridTemplateSmallScreens } from "@/utils/grid";
 
-type Props = {}
+type Props = {};
 
-const gridTemplate = `
-    "a b c"
-    "a b c"
-    "a b c"
-    "a b f"
-    "d e f"
-    "d e f"
-    "d h i"
-    "g h i"
-    "g h j"
-    "g h j"
 
-`;
 
 const Dashboard = (props: Props) => {
-    const { palette } = useTheme();
-  return (
-    <Box 
-    width="100%" 
-    height="100%" 
-    display="grid" 
-    gap="1.5rem"
-    sx={{
-        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-        gridTemplateAreas: gridTemplate,
-    }}
-    >
-        Dashboard
-    </Box>
-  )
-}
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
+  const { palette } = useTheme();
 
-export default Dashboard
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
+    >
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="b"></Box>
+      <Box bgcolor="#fff" gridArea="c"></Box>
+      <Box bgcolor="#fff" gridArea="d"></Box>
+      <Box bgcolor="#fff" gridArea="e"></Box>
+      <Box bgcolor="#fff" gridArea="f"></Box>
+      <Box bgcolor="#fff" gridArea="g"></Box>
+      <Box bgcolor="#fff" gridArea="h"></Box>
+      <Box bgcolor="#fff" gridArea="i"></Box>
+      <Box bgcolor="#fff" gridArea="j"></Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
